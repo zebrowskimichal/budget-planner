a1a3f525fb7ec78b1a9bd4aa13f97e51d930124d
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2023-01-04 22:10:04 +0100

    Included shops-checkbox.php scripts

--- index.php
+++ index.php
@@ -1,25 +1,29 @@
 <?php
     include "head.html";
     echo "<body>
-            <h1>Budget Planner</h1>
+            <a href=''><h1>Budget Planner</h1></a>
+            <!-- Form section -->
                 <label for='date'>Date:</label>
                 <input type='date' name='date' id='date' />
                 </br></br>
                 <label for='shop'>Shop:</label>";
                 echo "<select name='shop'>";
                 include 'shops.php';
-                
-                echo "<select>";
-                echo "<div><input type='button' value='add' onclick='addShop()' id='shopButton'><div>";
+                echo "</select>";
+                echo "<input type='button' value='Add' onclick='addShop()' id='shopButton' class='blue-buttons'>";
                 echo "</br></br>
                 <label for='total'>Total:</label>
                 <input type='number' name='total' id='total' />
                 </br></br>
-                <input type='submit' value='Save' onclick='shops()'/>
-            </br></br>";
-
+                <input type='submit' value='Save' onclick='save()'>
+                <a href=''><input type='button' value='Refresh data' class='blue-buttons'></a>
+                <input type='button' value='Show fiters' onclick='filters()' class='blue-buttons' id='filter-button'>
+                </br>";
+                include 'shops-checkbox.php';
+            echo "</br></br>
+            <!-- The budget planner data will be displayed here -->";
     include "load.php";
-         echo "<!-- The budget planner data will be displayed here -->";
+
     echo "
         </table>
         </body>
