480746fce89450bc34b7a638071edb77e70835e8
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2023-01-06 14:07:46 +0100

    Update insert.php
    Deleted $id variables in script, MySQL will carry the Queries without ids.

--- insert.php
+++ insert.php
@@ -1,11 +1,10 @@
 <?php
     if(isset($_GET)){
         include 'conn.php';
-        $id = $_GET['id'];
         $date = $_GET['date'];
         $shop = $_GET['shop'];
         $total = $_GET['total'];
-        mysqli_query($base, "INSERT INTO `data`(`id`, `date`, `shop`, `total`) VALUES ('$id','$date','$shop','$total');");
-        echo "Inserted value: '$id','$date','$shop','$total' ";
+        mysqli_query($base, "INSERT INTO `data`(`date`, `shop`, `total`) VALUES ('$date','$shop','$total');");
+        echo "Inserted value: '$date','$shop','$total' ";
     }
 ?>
